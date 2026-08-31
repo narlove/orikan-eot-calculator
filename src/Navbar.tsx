1a4b86e0b381f04e62503d1d8ed86a69510676f4
nathan <natey@outlook.com.au>
2026-08-31 21:37:56 +1000

    included react router, added navbar, added dashboard/homepage and descriptor, added github link referral, hinted at coming features

---
+++ src/Navbar.tsx
@@ -0,0 +1,50 @@
+import { NavLink, Outlet } from "react-router";
+import { FaGithub } from 'react-icons/fa6';
+import './Navbar.css';
+
+function Navbar()
+{
+    const navLinkClass = ({ isActive }: { isActive: boolean }) =>
+        `top-navbar__link${isActive ? ' top-navbar__link--active' : ''}`;
+
+    return (
+        <>
+            <nav className="top-navbar" aria-label="Main navigation">
+                <div className="top-navbar__inner">
+                    <NavLink to="/" end className="top-navbar__brand" aria-label="Return to homepage">
+                        <span className="brand-mark brand-mark--nav" aria-hidden="true">
+                            <img
+                                src="https://media.licdn.com/dms/image/v2/D560BAQGyRzsFQPYLnQ/company-logo_200_200/company-logo_200_200/0/1681192356762/orikan_logo?e=2147483647&v=beta&t=_GneGM8DToONXj7KBWQTAt93JxWvLGAserBjigijTOI"
+                                alt="Orikan logo"
+                                width="100%"
+                                height="100%"
+                                style={{ borderRadius: 10 }}
+                            />
+                        </span>
+                    </NavLink>
+
+                    <div className="top-navbar__links">
+                        <NavLink to="/" end className={navLinkClass}>Dashboard</NavLink>
+                        <NavLink to="/eot" className={navLinkClass}>EOT Calculator</NavLink>
+                        {/* <NavLink to="/ppp" className={navLinkClass}>PPP calculator</NavLink> */}
+                        <button className={navLinkClass({ isActive: false }).concat(" top-navbar__link--disabled")}>PPP Calculator</button>
+                        <button className={navLinkClass({ isActive: false }).concat(" top-navbar__link--disabled")}>Advanced Escalations</button>
+                        <a
+                            className="github-referral"
+                            href="https://github.com/narlove/orikan-eot-calculator"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="View project on GitHub"
+                        >
+                            <FaGithub />
+                        </a>
+                    </div>
+                </div>
+            </nav>
+
+            <Outlet />
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/TempHomepage.tsx
@@ -0,0 +1,30 @@
+export default function TempHomepage() {
+    return (
+        <main className="app-shell">
+            <section className="card">
+                <div className="card-inner">
+                    <h1>Hi Team! Welcome to Call Centre Tools</h1>
+                    <h2>Overview</h2>
+                    <p>
+                        If you've gotten this link and haven't just stumbled here from SEO, this is a little project I'm working on that
+                        ultimately attemps to add a few shortcuts to some of the more repetitive processes in Call Centre.
+
+                    </p>
+
+                    <p>
+                        I'm trying to get this fixed, uploaded, and maintained as much as possible, however, this project is entirely personal
+                        and is not at the request of any team, leadership, or company entity (despite the branding). In fact, there is a full
+                        individual entity disclosure available on my GitHub.
+                    </p>
+
+                    <p>
+                        If you're here, you're probably not a developer. If you have a bug, suggestion, or anything in between, shoot me a message
+                        on Teams and specify which page (EOT calc, PPP calc, etc.) the bug or suggestion relates to, and we'll have a discussion to see
+                        what I can improve. If you're a developer, because I know there are a few CS students in the team, feel free to raise a GitHub issue
+                        or pull request (this whole thing is open source!).
+                    </p>
+                </div>
+            </section>
+        </main>
+    );
+}
